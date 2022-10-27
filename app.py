a2a0976d1ef1a3a1631782e8a7cc6c22e2eadb54
Cyber666Fox <104504035+Cyber666Fox@users.noreply.github.com>
2022-10-27 20:59:43 +0800

    file fixed
    file fixed

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 import os
-import readline
 from typing import Optional, Union, Tuple
 
 from flask import Flask, request, jsonify, Response
@@ -13,22 +12,6 @@ BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 DATA_DIR = os.path.join(BASE_DIR, "data")
 
 
-def do_query(params):
-    with open(os.path.join(DATA_DIR, params["file_name"])) as f:
-        file_data = f.readline()
-
-    if params["cmd1"] == "filter":
-        result = filter(lambda record:params["value1"] in record, file_data)
-    elif params["cmd1"] == "map":
-        col_num = int(params["value1"])
-        result = map(lambda record:record.split()[col_num], file_data)
-    elif params["cmd1"] == "unique":
-        result = set(file_data)
-    elif params["cmd1"] == "sort":
-        reverse = params["value1"] == "desc"
-        result = sorted(file_data, reverse=reverse)
-    return result
-
 @app.route("/perform_query", methods=['POST'])
 def perform_query() -> Union[Response, Tuple[str, int]]:
     query: Optional[dict] = request.json
@@ -41,9 +24,6 @@ def perform_query() -> Union[Response, Tuple[str, int]]:
         value2: str = query["value2"]
     except KeyError:
         return " ", 400
-
-    if not os.path.exists(os.path.join(DATA_DIR, file_name)):
-        raise BadRequest
 
     chunk = get_cmd(query)
 
